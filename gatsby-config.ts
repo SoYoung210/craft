346fdd7ad457559cf70e7c566f7153156b2b6f45
SoYoung210 <ethdud1@gmail.com>
2022-10-23 00:32:16 +0900

    chore: image plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,11 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ['gatsby-plugin-vanilla-extract'],
+  plugins: [
+    'gatsby-plugin-vanilla-extract',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
 };
 
 export default config;
