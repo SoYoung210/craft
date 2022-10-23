91df4652a50d4016a6075221b63f2fbf7a371caa
SoYoung210 <ethdud1@gmail.com>
2022-10-23 14:22:15 +0900

    fix: content box style

--- src/components/content-box/ContentBox.tsx
+++ src/components/content-box/ContentBox.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 
-import { dotStyle, headerStyle, rootStyle } from './ContentBox.css';
+import { styled } from '../../../stitches.config';
 
 interface Props {
   title: ReactNode;
@@ -9,14 +9,64 @@ interface Props {
 
 export function ContentBox({ title, children }: Props) {
   return (
-    <div className={rootStyle}>
-      <div className={headerStyle}>
-        <div className={dotStyle} />
-        <div className={dotStyle} />
-        <div className={dotStyle} />
-        {title}
-      </div>
+    <Root>
+      <Header>
+        <Dot />
+        <Dot />
+        <Dot />
+        <TitleBar>{title}</TitleBar>
+      </Header>
       {children}
-    </div>
+    </Root>
   );
 }
+
+const Dot = styled('div', {
+  borderRadius: '50%',
+  backgroundColor: '#dee2e6',
+
+  width: 8,
+  height: 8,
+
+  marginRight: 6,
+  transition: 'background-color 0.15s ease',
+});
+
+const Root = styled('div', {
+  backgroundColor: '$white',
+  boxShadow: '$medium',
+  overflow: 'hidden',
+  borderRadius: '8px',
+  height: 300,
+
+  '&:hover': {
+    [`${Dot}:nth-child(1)`]: {
+      backgroundColor: '#FF5F57',
+    },
+    [`${Dot}:nth-child(2)`]: {
+      backgroundColor: '#FEBC2E',
+    },
+    [`${Dot}:nth-child(3)`]: {
+      backgroundColor: '#28C840',
+    },
+  },
+});
+
+const Header = styled('div', {
+  display: 'flex',
+  backgroundColor: '$gray1',
+  minHeight: 50,
+  alignItems: 'center',
+  padding: '0 16px',
+  color: '$gray7',
+});
+
+const TitleBar = styled('div', {
+  marginLeft: 8,
+  backgroundColor: '$white',
+  borderRadius: 4,
+  px: 16,
+  py: 8,
+
+  flexGrow: 1,
+});
