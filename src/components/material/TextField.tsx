acc1f0f36bddd4c3af9280fed12b809c63703d2b
SoYoung210 <ethdud1@gmail.com>
2022-11-28 17:45:09 +0900

    feat: material/TextField

---
+++ src/components/material/TextField.tsx
@@ -0,0 +1,33 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
+
+import { styled } from '../../../stitches.config';
+
+type Props = ComponentPropsWithoutRef<typeof Input>;
+
+const TextField = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  return (
+    <Root>
+      <Input ref={ref} {...props} />
+    </Root>
+  );
+});
+const Root = styled('div', {
+  br: 8,
+  boxShadow: '0 0 0 2px rgba(0, 0, 0, 0.15)',
+
+  py: 4,
+  px: 10,
+});
+
+const Input = styled('input', {
+  background: 'transparent',
+  outline: 'none',
+  appearance: 'none',
+  minWidth: 0,
+  border: 'none',
+
+  width: '100%',
+  height: '100%',
+});
+
+export default TextField;
