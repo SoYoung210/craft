41819cc31eda1acf4131faf57c0df2b3a61e5d10
SoYoung210 <ethdud1@gmail.com>
2022-12-06 17:39:16 +0900

    feat: add stack, flex component

---
+++ src/components/material/Stack.tsx
@@ -0,0 +1,56 @@
+import { ComponentPropsWithRef, forwardRef } from 'react';
+
+import { styled } from '../../../stitches.config';
+import useMergeCss from '../../hooks/useMergeCss';
+import { PrimitiveValue } from '../../utils/type';
+
+import Flex from './Flex';
+
+export interface StackProps
+  extends Omit<ComponentPropsWithRef<typeof StackStyleDiv>, 'direction'> {
+  gap: PrimitiveValue;
+  selector?: string;
+}
+const defaultSelector = '*:not([style*="display: none"]):not([hidden])';
+
+export const HStack = forwardRef<HTMLDivElement, StackProps>((props, ref) => {
+  const {
+    gap,
+    selector = defaultSelector,
+    css: cssFromProps,
+    ...hStackProps
+  } = props;
+  const css = useMergeCss(
+    {
+      [`& > ${selector} + ${selector}`]: {
+        marginLeft: gap,
+      },
+    },
+    cssFromProps
+  );
+
+  return <StackStyleDiv ref={ref} css={css} direction="row" {...hStackProps} />;
+});
+
+export const VStack = forwardRef<HTMLDivElement, StackProps>((props, ref) => {
+  const {
+    gap,
+    selector = defaultSelector,
+    css: cssFromProps,
+    ...hStackProps
+  } = props;
+  const css = useMergeCss(
+    {
+      [`& > ${selector} + ${selector}`]: {
+        marginLeft: gap,
+      },
+    },
+    cssFromProps
+  );
+
+  return (
+    <StackStyleDiv ref={ref} css={css} direction="column" {...hStackProps} />
+  );
+});
+
+const StackStyleDiv = styled(Flex);
