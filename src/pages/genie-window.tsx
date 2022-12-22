4f839900156383ca64049312998320fa06f29530
SoYoung210 <ethdud1@gmail.com>
2022-12-22 18:59:20 +0900

    fix: tuning genie window add add docs

--- src/pages/genie-window.tsx
+++ src/pages/genie-window.tsx
@@ -4,7 +4,7 @@ import anime from 'animejs';
 import Text from '../components/material/Text';
 import { styled } from '../../stitches.config';
 import PageLayout from '../components/layout/PageLayout';
-import { VStack } from '../components/material/Stack';
+import { HStack, VStack } from '../components/material/Stack';
 import Button from '../components/material/Button';
 
 const GENIE_EFFECT_SHAPE = {
@@ -27,6 +27,7 @@ export default function GenieWindow() {
   );
 
   const handleClose = useCallback(() => {
+    console.log('close');
     anime
       .timeline({
         easing: 'easeOutCubic',
@@ -37,24 +38,19 @@ export default function GenieWindow() {
         duration: 0,
       })
       .add({
-        targets: modalElement,
-        translateY: -15,
-        duration: 300,
-      })
-      .add({
         targets: pathElement,
         d: [
           {
             value: GENIE_EFFECT_SHAPE.STEP1,
           },
         ],
-        duration: 450,
+        duration: 350,
       })
       .add({
         targets: modalElement,
         matrix3d: MATRIX_TRANSFORM.SKEW,
         opacity: 0,
-        duration: 400,
+        duration: 600,
       })
       .add({
         targets: buttonElement,
@@ -70,6 +66,7 @@ export default function GenieWindow() {
       .add({
         targets: buttonElement,
         opacity: 0,
+        duration: 300,
       })
       .add({
         targets: modalElement,
@@ -101,13 +98,53 @@ export default function GenieWindow() {
     <PageLayout>
       <PageLayout.Title>Genie Window</PageLayout.Title>
       <PageLayout.Details>
-        <PageLayout.Summary>TBD</PageLayout.Summary>
-        <PageLayout.DetailsContent>TBD</PageLayout.DetailsContent>
+        <PageLayout.Summary>
+          clipPath + matrix3d transform (w.{' '}
+          <a href="https://animejs.com/" target="_blank" rel="noreferrer">
+            anime
+          </a>
+          )
+        </PageLayout.Summary>
+        <PageLayout.DetailsContent>
+          <ol>
+            <li>
+              anime을 활용하여{' '}
+              <a
+                href="https://animejs.com/documentation/#morphing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                svg morphing
+              </a>{' '}
+              (주의사항: svg morphing은 svg point개수가 같아야 하기 때문에,
+              피그마에서 하나의 object로 morphing된 여러개의 svg를 제작 및 사용)
+            </li>
+            <li>
+              하단 skew효과를 위해 matrix3d transform 적용 (참고:{' '}
+              <a
+                href="https://ramlmn.github.io/visualizing-matrix3d/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                visualizing-matrix3d
+              </a>
+              )
+            </li>
+          </ol>
+        </PageLayout.DetailsContent>
       </PageLayout.Details>
 
       <Modal ref={setModalElement}>
         <ModalBorder data-role="modal-border" />
+
         <ModalContent>
+          <DotButton variant="ghost" onClick={handleClose}>
+            <HStack gap={6}>
+              <Dot />
+              <Dot />
+              <Dot />
+            </HStack>
+          </DotButton>
           <VStack gap={32}>
             <Text color="#434343" size={20}>
               Modal Interaction with Genie Effect
@@ -116,32 +153,8 @@ export default function GenieWindow() {
               Inspired by Mac OS X minimize interaction.
             </Text>
           </VStack>
-          <button onClick={handleClose}>minimize</button>
         </ModalContent>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="502"
-          height="302"
-          viewBox="0 0 502 302"
-        >
-          <defs>
-            <clipPath id="clipping">
-              <path
-                ref={setPathElement}
-                fill="#FFF"
-                stroke="#979797"
-                // step 0
-                d="M251.745 0.5H496.605C499.45 0.5 501.103 2.65 500.295 5.3C500.295 5.3 500.295 229.5 500.295 370.5C406.47 370.5 339.306 370.5 339.306 370.5C343.783 370.5 333.637 370.5 330.79 370.5H172.696C169.849 370.5 133.833 370.5 129.356 370.5C129.356 370.5 85.077 370.5 0.5 370.5C0.499954 290.508 3.19297 5.3 3.19297 5.3C2.38522 2.65 4.03889 0.5 6.88409 0.5H251.745Z"
-                // step 1
-                // d="M250 0H496.086C498.945 0 500.606 2.15 499.794 4.8C499.794 4.8 475.603 91.046 403.538 179.622C331.471 268.197 334.636 364.992 334.636 364.992C334.601 367.768 332.303 370 329.441 370H170.556C167.695 370 165.396 367.768 165.362 364.992C165.362 364.992 168.524 268.197 96.4601 179.622C24.3959 91.046 0.205832 4.8 0.205832 4.8C-0.605964 2.15 1.05599 0 3.91542 0H250Z"
-                // step 2
-                // d="M251.745 0.5H496.605C499.45 0.5 501.103 2.65 500.295 5.3C500.295 5.3 500.295 229.5 500.295 370.5C406.47 370.5 339.306 370.5 339.306 370.5C343.783 370.5 333.637 370.5 330.79 370.5H172.696C169.849 370.5 133.833 370.5 129.356 370.5C129.356 370.5 85.077 370.5 0.5 370.5C0.499954 290.508 3.19297 5.3 3.19297 5.3C2.38522 2.65 4.03889 0.5 6.88409 0.5H251.745Z"
-              />
-            </clipPath>
-          </defs>
-        </svg>
       </Modal>
-
       <Button
         ref={setButtonElement}
         style={{ width: 120, marginLeft: 200, opacity: 0 }}
@@ -149,16 +162,33 @@ export default function GenieWindow() {
       >
         Open Modal
       </Button>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="502"
+        height="302"
+        viewBox="0 0 502 302"
+      >
+        <defs>
+          <clipPath id="clipping">
+            <path
+              ref={setPathElement}
+              fill="#FFF"
+              stroke="#979797"
+              d={GENIE_EFFECT_SHAPE.STEP0}
+            />
+          </clipPath>
+        </defs>
+      </svg>
     </PageLayout>
   );
 }
-// 110368588840
+
 const ModalBorder = styled('div', {
   position: 'absolute',
   top: 0,
   left: 1,
   width: 'calc(100% + 2px)',
-  height: 'calc(100% + 2px)',
+  height: 'calc(100% - 1px)',
   clipPath: 'url(#clipping)',
   borderRadius: 'inherit',
 
@@ -169,6 +199,8 @@ const ModalBorder = styled('div', {
 
 const Modal = styled('div', {
   position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
 
   width: 500,
   height: 300,
@@ -182,11 +214,40 @@ const Modal = styled('div', {
   transformOrigin: 'bottom',
 });
 
+const Dot = styled('div', {
+  borderRadius: '50%',
+  backgroundColor: '#dee2e6',
+
+  width: 8,
+  height: 8,
+});
+const DotButton = styled(Button, {
+  display: 'flex',
+  '&&': {
+    position: 'absolute',
+  },
+  top: '6%',
+  left: '5%',
+
+  background: 'transparent',
+
+  [`${Dot}:nth-child(1)`]: {
+    backgroundColor: '#FF5F57',
+  },
+  [`${Dot}:nth-child(2)`]: {
+    backgroundColor: '#FEBC2E',
+  },
+  [`${Dot}:nth-child(3)`]: {
+    backgroundColor: '#28C840',
+  },
+});
+
 const ModalContent = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
+  flexGrow: 1,
 
   backgroundColor: '$white',
   borderRadius: 'inherit',
@@ -198,4 +259,6 @@ const ModalContent = styled('div', {
 
   clipPath: 'url(#clipping)',
   transition: 'clip-path 1s',
+
+  position: 'relative',
 });
