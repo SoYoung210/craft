0f26e5af7e43541ce33d21a1b37472d841d97ef5
SoYoung210 <ethdud1@gmail.com>
2023-01-11 23:40:25 +0900

    feat: figrue component
    - layout

---
+++ src/components/layout/Figure.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+
+import { styled } from '../../../stitches.config';
+
+interface Props {
+  theme?: 'dark' | 'light';
+  children: ReactNode;
+}
+export default function Figure(props: Props) {
+  const { theme, children } = props;
+  return <Root theme={theme}>{children}</Root>;
+}
+
+const Root = styled('div', {
+  $$gridColor: 'rgb(244 244 245)',
+  $$bgColor: '#fff',
+
+  backgroundColor: '$$bgColor',
+  backgroundImage:
+    'linear-gradient(to right,$$gridColor 1px,#0000 1px),linear-gradient(to bottom,$$gridColor 1px,#0000 1px)',
+  '-webkit-mask-image': 'radial-gradient(white,black)',
+  backgroundSize: '2rem 2rem',
+
+  px: '1.5rem',
+  minHeight: '36rem',
+  borderRadius: '1rem',
+
+  variants: {
+    theme: {
+      dark: {
+        $$gridColor: 'rgba(39 39 42/0.25)',
+        $$bgColor: '#18181B',
+      },
+      light: {
+        // $$gridColor: 'rgb(244 244 245)',
+      },
+    },
+  },
+});
