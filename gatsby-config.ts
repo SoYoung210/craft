7a881ab79a759b473641eb68efc63c82510543e9
SoYoung210 <ethdud1@gmail.com>
2023-01-12 20:57:18 +0900

    chore: gatsby-transformer-sharp

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,6 +13,7 @@ const config: GatsbyConfig = {
     'gatsby-plugin-vanilla-extract',
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
