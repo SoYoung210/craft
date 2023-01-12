8beca711fa57eac81445cbdcd02a15bc734abf44
soso <ethdud1@gmail.com>
2023-01-12 23:35:42 +0900

    fix: main layout performance issue (#8)
    * chore: update peer-deps
    
    * chore: add missing eslint
    
    * fix: performance issue fix
    
    - change main layout to fixed

--- src/components/layout/PageLayout.css.ts
+++ src/components/layout/PageLayout.css.ts
@@ -1,14 +1,3 @@
-import { keyframes, style } from '@vanilla-extract/css';
-
-import { entries } from '../../utils/object';
-import { radialGradient } from '../../utils/style/gradient';
-
-export const backgroundAnimation = keyframes({
-  '0%': { backgroundPosition: '0% 50%' },
-  '50%': { backgroundPosition: '80% 100%' },
-  '100%': { backgroundPosition: '0% 50%' },
-});
-
 export const backgroundColorMap = {
   blue: {
     start: '50%',
@@ -46,41 +35,3 @@ export const backgroundColorMap = {
     value: '#eea5ba',
   },
 };
-
-export const pageStyles = style({
-  maxWidth: 760,
-  padding: 64,
-  minHeight: '100vh',
-
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '32px',
-
-  position: 'relative',
-  margin: '0 auto',
-
-  '::before': {
-    position: 'absolute',
-    content: '',
-    width: '100%',
-    height: '100%',
-
-    backgroundImage: entries(backgroundColorMap)
-      .map(([, { start, end, value }]) => {
-        return radialGradient(start, end, [`${value} 0`, 'transparent 50%']);
-      })
-      .join(', '),
-    backgroundSize: '180%, 200%',
-    filter: 'blur(100px) saturate(150%)',
-    animation: `${backgroundAnimation} infinite 10s linear`,
-    opacity: 0.2,
-    zIndex: -1,
-  },
-});
-
-export const headingStyles = style({
-  fontSize: 48,
-  fontWeight: 700,
-  color: '#000',
-  letterSpacing: '-0.03em',
-});
