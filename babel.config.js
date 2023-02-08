3c1ad6540e0244b3d0db16ba86c0b3ea31ac40e4
SoYoung210 <ethdud1@gmail.com>
2023-02-08 18:12:11 +0900

    chore: add babel preset loose:true

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+      },
+    ],
     [
       '@babel/preset-react',
       {
