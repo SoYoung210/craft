ebf0a452481d24b838a8b2d1f93e6900c0d490aa
SoYoung210 <ethdud1@gmail.com>
2023-02-14 21:02:06 +0900

    refactor: update useTimeout feature - start/clear/resume/pause

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -1,39 +1,48 @@
 import { useCallback, useEffect, useRef } from 'react';
 
-export default function useTimeout() {
-  const timeoutIds = useRef<Array<number>>([]);
+import useCallbackRef from './useCallbackRef';
 
-  const clearTimeout = useCallback(() => {
-    if (globalThis?.document == null) {
-      return;
-    }
+export default function useTimeout<T extends (...args: any[]) => any>(
+  callback: T | undefined
+) {
+  const timerRef = useRef(0);
+  const timerStartTimeRef = useRef(0);
+  const timerRemainingTimeRef = useRef(0);
 
-    if (timeoutIds.current.length > 0) {
-      timeoutIds.current.forEach(id => {
-        window.clearTimeout(id);
-      });
-    }
+  const callbackHandler = useCallbackRef(callback);
+
+  const start = useCallback(
+    (duration: number) => {
+      timerStartTimeRef.current = new Date().getTime();
+      timerRef.current = window.setTimeout(callbackHandler, duration);
+      // 시작시간 기록
+    },
+    [callbackHandler]
+  );
+  const clear = useCallback(() => {
+    window.clearTimeout(timerRef.current);
   }, []);
 
-  const setTimeout = useCallback((callback: () => void, delay: number) => {
-    if (globalThis?.document == null) {
-      return;
-    }
-
-    const newId = window.setTimeout(() => {
-      callback();
-      timeoutIds.current = timeoutIds.current.filter(id => id !== newId);
-    }, delay);
-
-    timeoutIds.current.push(newId);
+  const pause = useCallback(() => {
+    const elapsedTime = new Date().getTime() - timerStartTimeRef.current;
+    timerRemainingTimeRef.current = timerRemainingTimeRef.current - elapsedTime;
+    window.clearTimeout(timerRef.current);
   }, []);
+
+  const resume = useCallback(() => {
+    // 재시작시 기존 타이머 리셋하고, setTimeout(fn, 남은시간)으로 다시 시작
+    window.clearTimeout(timerRef.current);
+    start(timerRemainingTimeRef.current);
+  }, [start]);
 
   useEffect(() => {
-    return clearTimeout;
-  }, [clearTimeout]);
+    return clear;
+  }, [clear]);
 
   return {
-    setTimeout,
-    clearTimeout,
+    start,
+    clear,
+    resume,
+    pause,
   };
 }
