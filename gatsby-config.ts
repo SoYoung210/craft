3bd23249675c9335663cadc448b6b0deb944c1a1
SoYoung210 <ethdud1@gmail.com>
2023-02-21 21:29:36 +0900

    chore: add gatsby-plugin-filesystem

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,12 +5,22 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `craft`,
     siteUrl: `https://craft.so-so.dev/`,
+    description: `Build, Collect user interfaces of the future what is exciting and challenging to create.`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
