b7b6134cf958303a5d947fba991083996e0baa91
SoYoung210 <ethdud1@gmail.com>
2023-02-21 16:23:47 +0900

    fix: gatsby cname setup

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Uing`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `craft`,
+    siteUrl: `https://craft.so-so.dev/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
