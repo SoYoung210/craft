4b4340ce82619d858f39d0c9fbf1cc5a6c9007f5
SoYoung210 <ethdud1@gmail.com>
2023-03-05 18:02:07 +0900

    chore: jest-preview configuration

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testMatch: ['**/?(*.)+(test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': 'babel-jest',
+    '^.+\\.(css|scss|sass|less)$': 'jest-preview/transforms/css',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)':
+      'jest-preview/transforms/file',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+  ],
 };
