69bffa6a8d5f2e7ecb06fb028251139436eaa2c4
SoYoung210 <ethdud1@gmail.com>
2023-03-26 22:04:48 +0900

    chore: env

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,10 @@
 import type { GatsbyConfig } from 'gatsby';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 const config: GatsbyConfig = {
   polyfill: false,
   siteMetadata: {
