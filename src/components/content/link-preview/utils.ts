225a4acf6f205d0c936db26222e9f0c123784a76
SoYoung210 <ethdud1@gmail.com>
2023-04-09 21:06:46 +0900

    fix: fetch error throw

--- src/components/content/link-preview/utils.ts
+++ src/components/content/link-preview/utils.ts
@@ -2,12 +2,17 @@ import { isDevelopment } from '../../../utils/env';
 
 // NOTE: api 함수는 별도로 구성하는 것이 좋지만, 이 프로젝트에서 api를 쓸일이 그리 많지 않아보이므로 그냥 처리
 const BASE_PATH = isDevelopment() ? 'http://localhost:8888' : '';
-export const getScreenshot = async (url: string) => {
+export const getScreenshot = async (url: string, options?: RequestInit) => {
   const res = await fetch(`${BASE_PATH}/.netlify/functions/screenshot`, {
     method: 'POST',
     body: url,
     mode: 'no-cors',
+    ...options,
   });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch screenshot');
+  }
 
   const imageBlob = await res.blob();
   return URL.createObjectURL(imageBlob);
