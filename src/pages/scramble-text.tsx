0d779cc00e6d5e52acfb87ff3577e1787fd209d8
SoYoung210 <ethdud1@gmail.com>
2023-05-18 13:51:01 +0900

    chore: add reload button

--- src/pages/scramble-text.tsx
+++ src/pages/scramble-text.tsx
@@ -1,8 +1,14 @@
+import { useReducer } from 'react';
+
 import ScrambleContent from '../components/content/scramble-text/ScrambleContent';
 import Figure from '../components/layout/Figure';
 import PageLayout from '../components/layout/PageLayout';
+import Button from '../components/material/Button';
+import { RotateLeftIcon } from '../components/material/icon/RotateLeft';
 
 export default function ScrambleTextPage() {
+  const [key, increaseKey] = useReducer(state => state + 1, 0);
+
   return (
     <PageLayout>
       <PageLayout.Title>Scramble Text</PageLayout.Title>
@@ -15,19 +21,30 @@ export default function ScrambleTextPage() {
           alignItems: 'center',
           justifyContent: 'center',
           height: '300px',
+          position: 'relative',
         }}
       >
         <ScrambleContent
+          key={key}
           interval={30}
           style={{ fontSize: 18, fontWeight: 300 }}
         >
           <ScrambleContent.Text>
             Lorem ipsum dolor sit amet,
           </ScrambleContent.Text>
-          <ScrambleContent.Text style={{ marginLeft: 2 }}>
-            consectetur adipisicing elit.
+          <ScrambleContent.Text
+            style={{ marginLeft: 2, textDecoration: 'underline' }}
+          >
+            consec tetur adipisicing elit.
           </ScrambleContent.Text>
         </ScrambleContent>
+        <Button
+          aria-label="reload scramble text animation"
+          style={{ position: 'absolute', top: 10, right: 32 }}
+          onClick={increaseKey}
+        >
+          <RotateLeftIcon size={14} />
+        </Button>
       </Figure>
     </PageLayout>
   );
