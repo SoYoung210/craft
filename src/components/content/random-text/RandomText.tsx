d9f1152536183358b4570c5335239f4e99ca3475
SoYoung210 <ethdud1@gmail.com>
2023-05-21 15:00:14 +0900

    chore: comment

--- src/components/content/random-text/RandomText.tsx
+++ src/components/content/random-text/RandomText.tsx
@@ -18,9 +18,9 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 /**
  *
- * @description RadomText 하위에는 RandomText.Content컴포넌트로만 구성하는 것 추천
+ * @description RadomText 하위에는 RandomText.Block컴포넌트로만 구성하는 것을 권장합니다.
+ * Block이외의 컴포넌트를 사용해야 한다면 opacity를 직접 control해야 합니다.
  */
-
 export default function RandomText({
   children,
   interval = 30,
