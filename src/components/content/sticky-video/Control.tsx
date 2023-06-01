736993940c9d421bf5ad1771733ae975b32e5475
SoYoung210 <ethdud1@gmail.com>
2023-06-01 19:56:56 +0900

    feat: control

--- src/components/content/sticky-video/Control.tsx
+++ src/components/content/sticky-video/Control.tsx
@@ -1,24 +1,34 @@
+import { HTMLAttributes, useMemo } from 'react';
+
 import { styled } from '../../../../stitches.config';
 import { PauseIcon } from '../../material/icon/PauseIcon';
 import { PlayIcon } from '../../material/icon/Play';
 
-interface PlayControlProps {
+interface PlayControlProps extends HTMLAttributes<HTMLButtonElement> {
   playing: boolean;
   onPlayingChange: (playing: boolean) => void;
+  size?: number;
 }
 
 // const ICON_COLOR = 'white';
 const ICON_COLOR = 'gray0';
-export function PlayControl({ playing, onPlayingChange }: PlayControlProps) {
+export function PlayControl({
+  playing,
+  onPlayingChange,
+  size = 24,
+  ...restProps
+}: PlayControlProps) {
   const label = playing ? 'pause video' : 'play video';
   const handler = () => {
     onPlayingChange(!playing);
   };
-  const Icon = playing ? PauseIcon : PlayIcon;
+  const Icon = useMemo(() => {
+    return playing ? PauseIcon : PlayIcon;
+  }, [playing]);
 
   return (
-    <ResetButton aria-label={label} onClick={handler}>
-      <Icon color={ICON_COLOR} />
+    <ResetButton aria-label={label} onClick={handler} {...restProps}>
+      <Icon color={ICON_COLOR} size={size} />
     </ResetButton>
   );
 }
