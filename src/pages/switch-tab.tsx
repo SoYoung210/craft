59cdc2a33d65581165f911388353f5e91385bd19
SoYoung210 <ethdud1@gmail.com>
2023-08-12 17:00:36 +0900

    feat: useHotKey

---
+++ src/pages/switch-tab.tsx
@@ -0,0 +1,14 @@
+import PageLayout from '../components/layout/PageLayout';
+
+export default function SwitchTabPage() {
+  return (
+    <PageLayout>
+      <PageLayout.Title>Switch Tab</PageLayout.Title>
+      <PageLayout.Details>
+        <PageLayout.Summary>
+          Mac, Arc Style Switch Tab (use Shift + Tab)
+        </PageLayout.Summary>
+      </PageLayout.Details>
+    </PageLayout>
+  );
+}
