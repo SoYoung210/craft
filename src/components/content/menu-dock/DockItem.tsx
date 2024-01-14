5df2a3d186e3839e9d5990fd3c601f3771e783b3
SoYoung210 <ethdud1@gmail.com>
2024-01-14 23:19:57 +0900

    content setup

--- src/components/content/menu-dock/DockItem.tsx
+++ src/components/content/menu-dock/DockItem.tsx
@@ -1,5 +1,6 @@
+import { composeEventHandlers } from '@radix-ui/primitive';
 import { motion } from 'framer-motion';
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, HTMLAttributes, ReactNode } from 'react';
 
 import { styled } from '../../../../stitches.config';
 
@@ -12,13 +13,22 @@ import { useMenuDockContext } from './context';
 const x반축길이 = 220;
 const y반축길이 = 85;
 const 중심좌표 = 200;
-export interface DockItemProps {
+export interface DockItemProps
+  extends Omit<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    'onDrag' | 'onDragEnd' | 'onDragStart' | 'onAnimationStart'
+  > {
   children: ReactNode;
   index: number;
 }
 
 const origin = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-export function DockItem({ children, index }: DockItemProps) {
+export function DockItem({
+  children,
+  index,
+  onClick,
+  ...restProps
+}: DockItemProps) {
   const { onActiveIndexChange, degrees, activeIndex } =
     useMenuDockContext('DockItem');
 
@@ -41,9 +51,9 @@ export function DockItem({ children, index }: DockItemProps) {
 
   return (
     <Item
-      onClick={e => {
+      onClick={composeEventHandlers(onClick, () => {
         onActiveIndexChange(index);
-      }}
+      })}
       initial={{
         x,
         y,
@@ -58,6 +68,7 @@ export function DockItem({ children, index }: DockItemProps) {
         ease: [0.45, 0, 0.55, 1],
         duration: 0.18,
       }}
+      {...restProps}
     >
       {children}
     </Item>
@@ -66,15 +77,17 @@ export function DockItem({ children, index }: DockItemProps) {
 
 function generateEllipsePoint(degree: number, origin = 중심좌표) {
   const radian = (degree * Math.PI) / 180;
-  const x = Math.cos(radian) * x반축길이 + origin;
+  // const x = Math.cos(radian) * x반축길이 + origin;
+  const x = Math.cos(radian) * x반축길이 + 290;
   const y = Math.sin(radian) * y반축길이 + origin;
 
   return { x, y };
 }
 
-const Item = styled(motion.div, {
+const Item = styled(motion.button, {
   position: 'absolute',
 
+  background: 'transparent',
   borderRadius: 12,
   border: '1px solid rgba(255, 255, 255, 0.9)',
   height: 48,
--- src/pages/menu-dock.tsx
+++ src/pages/menu-dock.tsx
@@ -1,9 +1,49 @@
+import { AnimatePresence, motion, Variants } from 'framer-motion';
+import { useState } from 'react';
+
 import { GRADIENT_IMAGES } from '../components/content/menu-dock/constant';
 import { DockItem } from '../components/content/menu-dock/DockItem';
 import MenuDock from '../components/content/menu-dock/MenuDock';
 import PageLayout from '../components/layout/page-layout/PageLayout';
 
+const variants: Variants = {
+  enter: (direction: number) => {
+    return {
+      // x: direction > 0 ? 1000 : -1000,
+      rotateY: 18,
+      x: 280,
+      y: 10,
+      opacity: 0,
+      scale: 0.9,
+      transformPerspective: 400,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    y: 0,
+    rotateY: 0,
+    opacity: 1,
+    scale: 1,
+    transformPerspective: 400,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      // x: direction < 0 ? 1000 : -1000,
+      x: -280,
+      y: 10,
+      rotateY: -18,
+      opacity: 0,
+      scale: 0.9,
+      transformPerspective: 400,
+    };
+  },
+};
+
 export default function MenuDockPage() {
+  const [index, setIndex] = useState(0);
+
   return (
     <PageLayout>
       <PageLayout.Title>Dock</PageLayout.Title>
@@ -11,25 +51,62 @@ export default function MenuDockPage() {
         <PageLayout.Summary>-</PageLayout.Summary>
         <PageLayout.DetailsContent>-</PageLayout.DetailsContent>
       </PageLayout.Details>
-      <div style={{ height: 210, overflow: 'hidden' }}>
-        <MenuDock initialIndex={4}>
-          {GRADIENT_IMAGES.map((src, index) => {
-            return (
-              <DockItem key={index} index={index}>
-                <img
-                  src={src}
-                  alt=""
-                  style={{
-                    width: '50%',
-                    height: '50%',
-                    borderRadius: 9999,
-                    userSelect: 'none',
+      <div style={{ position: 'relative', height: 400, overflow: 'hidden' }}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <AnimatePresence initial={false}>
+            <motion.div
+              key={index}
+              // src={image[0]}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              style={{
+                height: 240,
+                width: 240,
+                border: '1px solid',
+                position: 'absolute',
+                boxShadow: '0 8px 20px 0 rgba(108, 79, 197, 0.44)',
+                borderRadius: 16,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+              exit="exit"
+              transition={{
+                ease: [0.45, 0, 0.55, 1],
+                duration: 0.45,
+              }}
+            >
+              {index}
+            </motion.div>
+          </AnimatePresence>
+        </div>
+        {/* 5개 기준한 값으로 변경 */}
+        <div style={{ position: 'absolute', top: 180 }}>
+          <MenuDock initialIndex={4}>
+            {GRADIENT_IMAGES.map((src, index) => {
+              return (
+                <DockItem
+                  key={index}
+                  index={index}
+                  onClick={() => {
+                    setIndex(prev => prev + 1);
                   }}
-                />
-              </DockItem>
-            );
-          })}
-        </MenuDock>
+                >
+                  <img
+                    src={src}
+                    alt=""
+                    style={{
+                      width: '50%',
+                      height: '50%',
+                      borderRadius: 9999,
+                      userSelect: 'none',
+                    }}
+                  />
+                </DockItem>
+              );
+            })}
+          </MenuDock>
+        </div>
       </div>
     </PageLayout>
   );
