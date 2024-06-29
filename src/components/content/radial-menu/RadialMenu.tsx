fe21cfeb8c84b0305b3c3554ea0237b61e1b402e
SoYoung210 <ethdud1@gmail.com>
2024-06-29 16:06:18 +0900

    path poc

--- src/components/content/radial-menu/RadialMenu.tsx
+++ src/components/content/radial-menu/RadialMenu.tsx
@@ -1,4 +1,12 @@
-import { Children, ComponentPropsWithoutRef, ReactNode, useCallback } from 'react';
+import {
+  Children,
+  ComponentPropsWithoutRef,
+  MouseEventHandler,
+  ReactNode,
+  useCallback,
+  useRef,
+  useState,
+} from 'react';
 import {
   motion,
   useMotionTemplate,
@@ -19,6 +27,13 @@ interface RadialMenuProps {
   children: ReactNode;
 }
 
+/**
+ * TODO:
+ * - [ ] 초기 떠있는 위치가 있고, 마우스를 클릭했을 때 그 위치에 다시 그려지면서
+ * - [ ] 마우스 움직임 따라 path가 그려지는 것
+ * - [ ] 마우스를 때면 선택된 아이템 정보를 담은 콜백을 실행할 것
+ * - [ ] (detail) dynamic card effect
+ */
 const ringPercent = 87.4;
 export function RadialMenu(props: RadialMenuProps) {
   const selectionBgAngle = useMotionValue(-1);
@@ -53,7 +68,9 @@ export function RadialMenu(props: RadialMenuProps) {
               </RadialMenuItemProvider>
             );
           })}
+          <InnerCircle />
         </RadialMenuProvider>
+        <LinePath />
       </Menu>
     </Root>
   );
@@ -67,9 +84,7 @@ export function RadialMenuItem(props: MenuItemProps) {
     useRadialMenuContext('RadialMenuItem');
   const { index } = useRadialMenuItemContext('RadialMenuItem');
   const angle = 45 * (index + 1) - 90;
-  const selectionAngle = (45 * index + 270) % 360;
   const { children } = props;
-  console.log(children, index, index);
 
   const getNextSelectionAngle = useCallback(() => {
     const currentAngle = selectionAngleMotionValue.get();
@@ -83,7 +98,7 @@ export function RadialMenuItem(props: MenuItemProps) {
     }
 
     return nextAngle;
-  },[])
+  }, [index, selectionAngleMotionValue]);
 
   return (
     <Item
@@ -116,6 +131,81 @@ const Root = styled('div', {
   width: 280,
   height: 280,
 });
+
+function InnerCircle() {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 140,
+        height: 140,
+        borderRadius: 999,
+        background: 'rgb(250,250,250)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          width: 132,
+          height: 132,
+          borderRadius: 'inherit',
+          border: '3px solid #fff',
+        }}
+      />
+    </div>
+  );
+}
+
+function LinePath() {
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  const [startPos, setStartPos] = useState({ x: 0, y: 0 });
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const handleMouseDown: MouseEventHandler<SVGSVGElement> = useCallback(e => {
+    setStartPos({ x: e.clientX, y: e.clientY });
+  }, []);
+
+  const handleMouseMove: MouseEventHandler<SVGSVGElement> = useCallback(e => {
+    setMousePos({ x: e.clientX, y: e.clientY });
+  }, []);
+
+  const pathD =
+    startPos.x !== 0
+      ? `M ${startPos.x},${startPos.y} L ${mousePos.x},${mousePos.y}`
+      : '';
+
+  return (
+    <svg
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      ref={svgRef}
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        // pointerEvents: 'none',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <path
+        d={pathD}
+        stroke="black"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{
+          stroke: 'rgba(0, 0, 0, 0.05)',
+        }}
+      />
+    </svg>
+  );
+}
 
 function Shadow(props: ComponentPropsWithoutRef<typeof motion.div>) {
   return (
