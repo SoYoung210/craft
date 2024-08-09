1009a3caac16b7fbdd12c9f54c5134c28b1ab6e0
SoYoung210 <ethdud1@gmail.com>
2024-08-09 19:04:04 +0900

    refactor createContext

--- src/components/utility/createContext.tsx
+++ src/components/utility/createContext.tsx
@@ -5,6 +5,17 @@ import {
   useMemo,
 } from 'react';
 
+export class ContextError extends Error {
+  static ErrorName = 'InvalidContextUsage';
+  static isContextError(error: Error) {
+    return error.name === ContextError.ErrorName;
+  }
+
+  constructor(message: string) {
+    super(message);
+    this.name = ContextError.ErrorName;
+  }
+}
 export function createContext<ContextValueType extends object | null>(
   rootComponentName: string,
   defaultContext?: ContextValueType
@@ -16,7 +27,11 @@ export function createContext<ContextValueType extends object | null>(
   function Provider(props: PropsWithChildren<ContextValueType>) {
     const { children, ...context } = props;
 
-    const value = useMemo(() => context, [context]) as ContextValueType;
+    const value = useMemo(
+      () => context,
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      Object.values(context)
+    ) as ContextValueType;
 
     return <Context.Provider value={value}>{children}</Context.Provider>;
   }
@@ -24,7 +39,7 @@ export function createContext<ContextValueType extends object | null>(
   function useContext(consumerName: string) {
     const context = useContextRaw(Context);
     if (context == null) {
-      throw new Error(
+      throw new ContextError(
         `${consumerName}은 ${rootComponentName}하위에서 사용해야 합니다.`
       );
     }
