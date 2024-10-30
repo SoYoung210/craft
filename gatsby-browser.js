108f0c27943b1c23cfbc4cf082388b9aa7b61a68
SoYoung210 <ethdud1@gmail.com>
2024-10-30 17:22:43 +0900

    chore: add nanum pen script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,5 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
+
+import '@fontsource/nanum-pen-script';
