4979aed726f46c23889409b67db13636683bb938
SoYoung210 <ethdud1@gmail.com>
2025-01-19 22:38:34 -0800

    setup tailwindcss

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,6 +17,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    'gatsby-plugin-postcss',
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
