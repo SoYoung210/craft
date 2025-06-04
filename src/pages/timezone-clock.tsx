bdb2c106aa44dad0c9fa00218459cd3953f25714
SoYoung210 <ethdud1@gmail.com>
2025-06-04 17:08:53 +0900

    remove set live time

--- src/pages/timezone-clock.tsx
+++ src/pages/timezone-clock.tsx
@@ -1,23 +1,16 @@
 import { useState, useCallback } from 'react';
-import { RotateCcw } from 'lucide-react';
 
 const SEOUL_TZ = 'Asia/Seoul';
 const SF_TZ = 'America/Los_Angeles';
 import PageLayout from '../components/layout/page-layout/PageLayout';
 import useInterval from '../hooks/useInterval';
 import { Clock } from '../components/content/timezone-clock/clock';
-import Button from '../components/material/Button';
 
 export default function TimezoneClockPage() {
   const [baseTime, setBaseTime] = useState(() => new Date());
-  const [isLiveTime, setIsLiveTime] = useState(true);
 
   useInterval(() => {
-    if (isLiveTime) {
-      setBaseTime(new Date());
-    } else {
-      setBaseTime(prevTime => new Date(prevTime.getTime() + 1000));
-    }
+    setBaseTime(prevTime => new Date(prevTime.getTime() + 1000));
   }, 1000);
 
   const handleTimeAdjust = useCallback((minuteAdjustment: number) => {
@@ -25,12 +18,6 @@ export default function TimezoneClockPage() {
       const newTime = new Date(prevTime.getTime() + minuteAdjustment * 60000);
       return newTime;
     });
-    setIsLiveTime(false);
-  }, []);
-
-  const resetToLiveTime = useCallback(() => {
-    setIsLiveTime(true);
-    setBaseTime(new Date());
   }, []);
 
   return (
@@ -51,17 +38,6 @@ export default function TimezoneClockPage() {
           onTimeAdjust={handleTimeAdjust}
         />
       </div>
-
-      {!isLiveTime && (
-        <Button
-          onClick={resetToLiveTime}
-          variant="outline"
-          className="shadow-md"
-        >
-          <RotateCcw className="mr-2 h-5 w-5" />
-          Reset to Live Time
-        </Button>
-      )}
     </PageLayout>
   );
 }
