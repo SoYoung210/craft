dc5c766d32b79602968a41a742adcdd339db7981
SoYoung210 <ethdud1@gmail.com>
2025-11-01 22:38:10 -0700

    fix pkg import

--- src/components/content/team-clock/remote-team-clock.tsx
+++ src/components/content/team-clock/remote-team-clock.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'motion/react';
 
 import { Switch } from '../../material/switch';
 import soyoungAvatar from '../../../images/remote-team-clock/soyoung.webp';
