761de001b52a763107d0c3ac93e8ea9be832dbc8
SoYoung210 <ethdud1@gmail.com>
2026-04-04 00:29:08 +0900

    fix deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
